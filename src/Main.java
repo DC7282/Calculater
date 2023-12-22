d68cb7c9e6790cb8d9f2872dd93024aa46e45a65
Dhiraj Chaudhary <dhirajchaudhary989@gmail.com>
2023-12-22 09:27:43 +0530

    Add UC1 : Addition feture added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 //
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter first number");
+        double input1=sc.nextDouble();
+        System.out.println("Enter Second number");
+        double input2= sc.nextDouble();
+        System.out.println("Addition is " +(input1+input2));
     }
 }
