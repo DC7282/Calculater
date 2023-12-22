039e127514c3981d4ed735196bb9cbc16d7ab601
Dhiraj Chaudhary <dhirajchaudhary989@gmail.com>
2023-12-22 09:37:14 +0530

    add UC3 : Multeplication feature added

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,6 @@ public class Main {
         double input2= sc.nextDouble();
         System.out.println("Addition is " +(input1+input2));
         System.out.println("Substraction is " +(input1-input2));
+        System.out.println("Multiplication is " + (input1*input2));
     }
 }
